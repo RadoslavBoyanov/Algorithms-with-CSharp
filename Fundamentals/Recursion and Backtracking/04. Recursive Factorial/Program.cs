7a6df39a13ca3553e80d6f77f78f5256dd331f75
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-11-01 19:13:27 +0200

    Recursion and Backtracking

---
+++ Fundamentals/Recursion and Backtracking/04. Recursive Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace _04._Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(number));
+        }
+
+        private static int Factorial(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
---
+++ Fundamentals/Recursion and Backtracking/07. Recursive Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _07._Recursive_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var fibonacci = new List<int>();
+            fibonacci.Add(0);
+            fibonacci.Add(1);
+
+            while (fibonacci.Count < n + 2)
+            {
+                var newFibonacci = fibonacci[^1] + fibonacci[^2];
+                fibonacci.Add(newFibonacci);
+            }
+
+            Console.WriteLine(fibonacci[^1]);
+        }
+
+        private static int GetFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+
+
+    }
+}
