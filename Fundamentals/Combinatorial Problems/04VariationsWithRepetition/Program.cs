6b542e93ad4432b0a58d67f08f4cd413342631bb
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-11-04 21:19:33 +0200

    Combinatorial problems

---
+++ Fundamentals/Combinatorial Problems/04VariationsWithRepetition/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace _04VariationsWithRepetition
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static int k;
+        private static string[] variations;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+
+            k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+
+            Variation(0);
+        }
+
+        private static void Variation(int index)
+        {
+            if (index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                variations[index] = elements[i];
+                Variation(index + 1);
+            }
+        }
+    }
+}
---
+++ Fundamentals/Combinatorial Problems/05CombinationsWithoutRep/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05CombinationsWithoutRep
+{
+    public class Program
+    {
+        private static string[] elements;
+        private static string[] combinations;
+        
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+
+            int k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            Combination(0, 0);
+        }
+
+        private static void Combination(int index, int startIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = startIndex; i < elements.Length; i++)
+            {
+                combinations[index] = elements[i];
+                Combination(index + 1, i + 1);
+            }
+        }
+    }
+}
