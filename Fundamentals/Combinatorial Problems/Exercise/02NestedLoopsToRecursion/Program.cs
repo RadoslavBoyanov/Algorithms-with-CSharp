2c57f37cc444c265125166528426ca0c42abcdda
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-11-11 16:15:15 +0200

    Combinatorial problems -exercise

---
+++ Fundamentals/Combinatorial Problems/Exercise/02NestedLoopsToRecursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace _02NestedLoopsToRecursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var array = new int[n];
+
+            NestLoop(array, 0);
+        }
+
+        private static void NestLoop(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                Console.WriteLine(string.Join(" ", array));
+                return;
+            }
+
+            for (int i = 1; i <= array.Length; i++)
+            {
+                array[index] = i;
+                NestLoop(array, index + 1);
+            }
+        }
+    }
+}
